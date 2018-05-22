e5f8f5f6f3d9555b7d8890b427907eb6244c5259
gbasile001 <gaetan.basile.1@etu.u-bordeaux.fr>
2018-05-22 11:52:02 +0900

    promise-based data importation tool finished for now

--- importContent.js
+++ importContent.js
@@ -13,14 +13,14 @@ class Answer {
         this.id = id;
     }
 
-    insertSelfInDatabase(dbcon, questionID) {
+    insertSelfInDatabase(dbCon, questionID) {
         let localId = this.id;
         let localText = this.text;
 
         let answerInsertSQL = `INSERT INTO answers(answer_id, answer_string, answer_questioncode)
         VALUES(${localId}, '${localText}', ${questionID})`;
 
-        dbcon.query(answerInsertSQL, function (err, result) {
+        dbCon.query(answerInsertSQL, function (err) {
             if (err) throw err;
             console.log(`1 answer inserted : "${localText}"`);
         });
@@ -33,74 +33,48 @@ class Answer {
 class Question {
     constructor(question, answers, rightAnswer) {
         this.question = question;
-        this.answers = answers;
+        this.answers = [];
         this.rightAnswer = rightAnswer;
+        answers.forEach(value => {
+            this.answers.push(new Answer(value.text, value.id));
+        }, this)
     }
 
-    insertSelfInDatabase(dbcon, sentenceID) {
-
+    insertSelfInDatabase(dbCon, sentenceID) {
         let localQuestion = this.question;
         let localRightAnswer = this.rightAnswer;
-        let localAnswers = this.answers;
-
-        let questionInsertSQL = `INSERT INTO questions(question_string, question_rightanswer, question_sentencecode)
+        return new Promise((resolve, reject) => {
+            let questionInsertSQL = `INSERT INTO questions(question_string, question_rightanswer, question_sentencecode)
             VALUES('${localQuestion}', ${localRightAnswer}, ${sentenceID})`;
 
-        dbcon.query(questionInsertSQL, function (err, result) {
-            if (err) throw err;
-            console.log(`1 question inserted : "${localQuestion}"`);
-            let questionID = result.insertId;
-
-            localAnswers.forEach(value => {
-                let answer = new Answer(value.text, value.id);
-                answer.insertSelfInDatabase(dbcon, questionID);
-            });
-            /*
-            dbcon.query(`SELECT LAST_INSERT_ID() AS last`, function (err, result) {
-                if (err) throw err;
-                questionID = result[0].last;
-
-                localAnswers.forEach(value => {
-                    let answer = new Answer(value.text, value.id);
-                    answer.insertSelfInDatabase(dbcon, questionID);
-                });
-            });*/
+            dbCon.query(questionInsertSQL, function (err, result) {
+                if (err) return reject(err);
+                console.log(`1 question inserted : "${localQuestion}"`);
+                resolve(result);
+            })
         });
     }
 }
 
-/**
- * Permits importing sentences in the database.
- */
-class DatabaseSentenceInserter {
-    constructor(sentence, questions, dbcon) {
+class Sentence {
+    constructor(sentence, questions) {
         this.sentence = sentence;
-        this.questions = questions;
-        this.dbcon = dbcon;
+        this.questions = [];
+        questions.forEach(value => {
+            this.questions.push(new Question(value.question, value.answers, value.rightAnswer));
+        }, this);
     }
 
-    insertSentenceInDatabase() {
-        //TODO find something better than sending a query via string
+    insertSelfInDatabase(dbCon) {
         let localSentence = this.sentence;
-        let localQuestions = this.questions;
-        let localCon = this.dbcon;
+        return new Promise((resolve, reject) => {
+            let sentenceInsertSQl = `INSERT INTO sentences(sentence_string)
+            VALUES('${localSentence}')`;
 
-        let sentenceInsertSQL = `INSERT INTO sentences(sentence_string) VALUES('${localSentence}')`;
-
-        // insert the sentence
-        localCon.query(sentenceInsertSQL, function (err, result) {
-            if (err) throw err;
-            console.log(`1 sentence inserted : "${localSentence}"`);
-
-            let insertedSentenceID = result.insertId;
-            localCon.query(`SELECT LAST_INSERT_ID() AS last`, function (err, result) {
-                if (err) throw err;
-                insertedSentenceID = result[0].last;
-
-                localQuestions.forEach(value => {
-                    let question = new Question(value.question, value.answers, value.rightAnswer);
-                    question.insertSelfInDatabase(localCon, insertedSentenceID);
-                });
+            dbCon.query(sentenceInsertSQl, function (err, result) {
+                if (err) return reject(err);
+                console.log(`1 sentence inserted : "${localSentence}"`);
+                resolve(result);
             });
         });
     }
@@ -111,8 +85,6 @@ class DatabaseSentenceInserter {
 // remove two first args (node executable and the path to this JS file)
 process.argv.shift();
 process.argv.shift();
-
-console.log(process.argv);
 
 // has to be 3 args
 if (process.argv.length < 3)
@@ -141,9 +113,6 @@ if (sentenceGraphRep === undefined || sentenceDRSRep === undefined)
 let sentenceString = sentenceJson.sentence;
 let sentenceQuestions = sentenceJson.questions;
 
-console.log(sentenceString);
-console.log(sentenceQuestions);
-
 // connection to the database
 let con = mysql.createConnection({
     host: config.mysql_host,
@@ -155,10 +124,30 @@ let con = mysql.createConnection({
 // connect to the database, throw the error if there is one
 con.connect(function (err) {
     if (err) throw err;
-
     console.log("Connected to mysql database.");
-
-    let dbInserter = new DatabaseSentenceInserter(sentenceString, sentenceQuestions, con);
-    dbInserter.insertSentenceInDatabase();
-    //con.end();
+    /*
+    insert sentence->promise->then while there are questions->insert question->promise then insert answers->wait for all
+    */
+    let insertSentence = new Sentence(sentenceString, sentenceQuestions);
+    let promises = [];
+    insertSentence.insertSelfInDatabase(con)
+        .then(sentenceInsertResult => {
+            let sentenceId = sentenceInsertResult.insertId;
+            insertSentence.questions.forEach(question => {
+                let actualQuestion = question;
+                promises.push(actualQuestion.insertSelfInDatabase(con, sentenceId)
+                    .then(questionInsertResult => {
+                        let questionId = questionInsertResult.insertId;
+                        actualQuestion.answers.forEach(answer => {
+                            answer.insertSelfInDatabase(con, questionId);
+                        })
+                    }));
+            });
+            return Promise.all(promises);
+        }).then(() => {
+        con.end();
+    }).catch(reason => {
+        con.end();
+        throw reason;
+    });
 });
