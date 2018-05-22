addb925655bbd03e95ff7d956f20e596d3dd356c
tguesdon <thomas.guesdon12@gmail.com>
2018-05-22 11:34:52 +0900

    Button test now go to the test page & created a js script that get and print all the questions from the db

---
+++ public/javascript/get_questions.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host : config.mysql_host,
+    user: config.mysql_username,
+    password: config.mysql_password, 
+    database:"visualization"
+});
+
+con.connect(function(err){
+    if(err) throw err;
+    con.query("SELECT * FROM questions",function(err,result,fields){
+        if(err) throw err;
+        console.log(result);
+    });
+});
