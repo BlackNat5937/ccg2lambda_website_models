b646d4ca804bda49283f8f3fd0dded326b4fa7f6
gbasile001 <gaetan.basile.1@etu.u-bordeaux.fr>
2018-05-24 15:20:11 +0900

    remade test page

--- routes/test.js
+++ routes/test.js
@@ -6,7 +6,7 @@ const config = require(`../config.json`);
 
 const fs = require('fs');
 
-function getDB(dbCon) {
+function getRandomQuestionFromDatabase(dbCon) {
     return new Promise((resolve, reject) => {
         dbCon.query("SELECT * FROM questions ORDER BY RAND() LIMIT 1", function (err, result, fields) {
             if (err) reject(err);
@@ -15,7 +15,7 @@ function getDB(dbCon) {
     });
 }
 
-function getAnswersDB(dbCon, codeQuestion){
+function getAnswersFromDatabse(dbCon, codeQuestion) {
     return new Promise((resolve, reject) => {
         dbCon.query("SELECT * FROM answers WHERE answers.answer_questioncode = " + codeQuestion, function (err, result2, fields) {
             if (err) reject(err);
@@ -37,53 +37,52 @@ router.get('/', function (req, res, next) {
 
 router.get('/test', function (req, res, next) {
     //session count page
-    if(typeof req.session.view === 'undefined'){
+    if (typeof req.session.view === 'undefined') {
         req.session.view = 1;
-    }else{
-        req.session.view ++;
+    } else {
+        req.session.view++;
     }
-    
-    if(req.session.view <= 10){
+
+    if (req.session.view <= 10) {
         let questions;
-    
+
         let con = mysql.createConnection({
             host: config.mysql_host,
             user: config.mysql_username,
             password: config.mysql_password,
             database: "visualization"
         });
-    
-        let random_question = "";
-        let object_question;
-        let answer = "";
-    
-        getDB(con).then(result => {
-            random_question = result[0].question_string;
-            object_question = result[0];
-            //con.end();
-        }).then(()=>{
-            return getAnswersDB(con, object_question.question_code).then(result2 => {
-                console.log("Code " + object_question.question_code)
-                answer = result2;
+
+        let randomQuestion = "";
+        let questionObject;
+        let answers = [];
+        let visualization;
+
+        getRandomQuestionFromDatabase(con).then(sentenceResult => {
+            randomQuestion = sentenceResult[0].question_string;
+            questionObject = sentenceResult[0];
+        }).then(() => {
+            return getAnswersFromDatabse(con, questionObject.question_code).then(answersResult => {
+                answers = answersResult;
             });
-        }).then(()=>{
+        }).then(() => {
             res.render('test', {
                 questions: questions,
-                question: random_question,
-                answer1: answer[0].answer_string,
-                answer2: answer[1].answer_string,
-                answer3: answer[2].answer_string,
+                question: randomQuestion,
+                answer1: answers[0].answer_string,
+                answer2: answers[1].answer_string,
+                answer3: answers[2].answer_string,
                 title: 'MR Test',
                 current: 'test',
                 number_test: req.session.view,
             });
         }).catch();
     }
-    else{
+    else {
         req.session.view = 0;
         res.redirect("/test/")
     }
-    
+
 });
 
 module.exports = router;
