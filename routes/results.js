e76f2f3d5eee5d365525b5f765c3d540c543beb7
tguesdon <thomas.guesdon12@gmail.com>
2018-05-29 15:40:44 +0900

    finished results javascript

--- routes/results.js
+++ routes/results.js
@@ -70,6 +70,107 @@ function getAllSentences(dbCon){
     });
 }
 
+function getSentence(dbCon, code){
+    return new Promise((resolve, reject) => {
+        dbCon.query("SELECT * FROM sentences WHERE sentences.sentence_code = " + code, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+}
+
+function getDrsSentenceSuccess(dbCon, code){
+    return new Promise((resolve, reject) => {
+        dbCon.query("SELECT COUNT(*) AS successCount FROM results INNER JOIN questions ON results_questioncode = questions.question_code INNER JOIN sentences ON questions.question_sentencecode = sentences.sentence_code WHERE results_isright = 'yes' AND results_visualizationtype = 'drs' AND sentences.sentence_code = " + code, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+}
+
+function getDrsSentenceTotal(dbCon, code){
+    return new Promise((resolve, reject) => {
+        dbCon.query("SELECT COUNT(*) AS total FROM results INNER JOIN questions ON results_questioncode = questions.question_code INNER JOIN sentences ON questions.question_sentencecode = sentences.sentence_code WHERE results_visualizationtype = 'drs' AND sentences.sentence_code = " + code, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+}
+
+function getGraphSentenceSuccess(dbCon, code){
+    return new Promise((resolve, reject) => {
+        dbCon.query("SELECT COUNT(*) AS successCount FROM results INNER JOIN questions ON results_questioncode = questions.question_code INNER JOIN sentences ON questions.question_sentencecode = sentences.sentence_code WHERE results_isright = 'yes' AND results_visualizationtype = 'graph' AND sentences.sentence_code = " + code, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+}
+
+function getGraphSentenceTotal(dbCon, code){
+    return new Promise((resolve, reject) => {
+        dbCon.query("SELECT COUNT(*) AS total FROM results INNER JOIN questions ON results_questioncode = questions.question_code INNER JOIN sentences ON questions.question_sentencecode = sentences.sentence_code WHERE results_visualizationtype = 'graph' AND sentences.sentence_code = " + code, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+}
+
+router.param('code_sentence', (req, res, next, code) => {
+    req.code_sentence = code;
+        next();
+    }
+);
+
+router.get('/sentence/:code_sentence', function(req, res, next){
+    let con = mysql.createConnection({
+        host: config.mysql_host,
+        user: config.mysql_username,
+        password: config.mysql_password,
+        database: "visualization"
+    });
+
+    let sentence_string;
+
+    let sentenceDrsSuccess;
+    let sentenceDrsTotal;
+    let sentenceGraphSuccess;
+    let sentenceGraphTotal;
+
+    getSentence(con, req.code_sentence).then(getSentenceResult => {
+        sentence_string = getSentenceResult[0].sentence_string;
+    }).then(() => {
+        return getDrsSentenceSuccess(con, req.code_sentence).then(drsSentenceSuccessResult => {
+            sentenceDrsSuccess = drsSentenceSuccessResult[0].successCount;
+        });
+    }).then(() => {
+        return getDrsSentenceTotal(con, req.code_sentence).then(drsSentenceTotalResult => {
+            sentenceDrsTotal = drsSentenceTotalResult[0].total;
+        });
+    }).then(() => {
+        return getGraphSentenceSuccess(con, req.code_sentence).then(graphSentenceSuccessResult => {
+            sentenceGraphSuccess = graphSentenceSuccessResult[0].successCount;
+        });
+    }).then(() => {
+        return getGraphSentenceTotal(con, req.code_sentence).then(graphSentenceTotalResult => {
+            sentenceGraphTotal = graphSentenceTotalResult[0].total;
+        });
+    }).then(() => {
+        res.render('question_result', {
+            title: 'Question results',
+            current: 'results',
+            sentence: sentence_string,
+            drsSuccess: sentenceDrsSuccess,
+            drsTotal: sentenceDrsTotal,
+            drsPercentage: getPercentage(sentenceDrsSuccess, sentenceDrsTotal),
+            graphSuccess: sentenceGraphSuccess,
+            graphTotal: sentenceGraphTotal,
+            graphPercentage: getPercentage(sentenceGraphSuccess, sentenceGraphTotal),
+        });
+    })
+    
+    
+});
+
 /**
  * GET home page
  */
@@ -88,6 +189,7 @@ router.get('/', function (req, res, next) {
     let totalGraph;
 
     let allSentences;
+
     getGlobalSuccess(con).then(globalSuccessResult => {
         globalSuccess = globalSuccessResult[0].successCount;
     }).then(()=>{
